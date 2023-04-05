5503876354a4a3e8d67559e510633350a7bf850f
blokyk <eliot.courvoisier@gmail.com>
2023-04-05 09:19:42 +0200

    feat(hs): implement Bit type

--- lib-chloride/src/Bit.hs
+++ lib-chloride/src/Bit.hs
@@ -1,6 +1,8 @@
 module Bit where
 
 import Algebra.Group
+import Algebra.Ring
+import Algebra.Field
 
 newtype Bit = Bit Bool deriving Eq
 
@@ -8,3 +10,14 @@ instance Group Bit where
     unit = Bit False
     inverse = id
     operation a b = Bit (a /= b) -- xor <=> !=
+
+instance Ring Bit where
+    add = operation
+    zero = Bit False
+    add_inverse = id
+    mult (Bit a) (Bit b) = Bit (a && b)
+    one = Bit True
+
+instance Field Bit where
+    mult_inverse (Bit True) = Bit True
+    mult_inverse _          = undefined
