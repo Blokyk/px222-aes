935468839f8b0bb24aa1dcb7629379c951e8bd3d
blokyk <eliot.courvoisier@gmail.com>
2023-04-05 23:35:53 +0200

    feat(hs): implement bytes using polynomials

---
+++ lib-chloride/src/Byte.hs
@@ -0,0 +1,35 @@
+module Byte (
+    byteFromPolynomial,
+    byte,
+    asBits
+) where
+
+import Bit
+
+import Algebra
+
+-- we don't wanna export the constructor cause its kinda
+-- unwieldy and means users could make "bytes" of any length
+newtype Byte = Byte (Polynomial Bit) deriving Eq
+
+byteFromPolynomial :: Polynomial Bit -> Byte
+byteFromPolynomial (Polynomial a) = byte a
+
+byte :: [Bit] -> Byte
+byte bits
+    | length bits > 8 = error "Can't make a byte from more than 8 bits!"
+    | otherwise       = Byte $ polynomial bits
+
+asBits :: Byte -> [Bit]
+asBits (Byte bits) = coeffs bits
+
+irreducibleByte :: Field a => Polynomial a
+--                          x^0  x^1        x^3  x^4                    x^8
+irreducibleByte = Polynomial [one, one, zero, one, one, zero, zero, zero, one]
+
+instance Ring Byte where
+    zero = Byte zero
+    one  = Byte one
+    add (Byte p) (Byte q)  = Byte (add p q)
+    add_inverse (Byte p)   = Byte (add_inverse p)
+    mult (Byte p) (Byte q) = Byte (multPolynomial p q `polyMod` irreducibleByte)
