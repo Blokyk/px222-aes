4183323eb70a99c5aaf6ebc2d8145a0a4b429993
blokyk <eliot.courvoisier@gmail.com>
2023-04-05 11:04:39 +0200

    chore: do stuff in Main

--- lib-chloride/app/Main.hs
+++ lib-chloride/app/Main.hs
@@ -1,6 +1,10 @@
 module Main (main) where
 
-import Algebra ()
+import Algebra.Polynomial
+import Bit
+
+p :: Polynomial Bit
+p = Polynomial [one, zero, zero, one, one, one, one, zero, one, one, zero, one]
 
 main :: IO ()
-main = putStrLn "Hello, AES!"
+main = print (divEuclide p irreducible_m)
