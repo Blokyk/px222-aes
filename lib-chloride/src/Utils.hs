df41510835dd209283af76939259656bec35b262
blokyk <eliot.courvoisier@gmail.com>
2023-04-05 09:18:17 +0200

    feat(hs): add Utils.hs

---
+++ lib-chloride/src/Utils.hs
@@ -0,0 +1,10 @@
+module Utils (
+    zipWithDefault,
+) where
+
+-- Same as zipWith, but instead of stopping when either of the
+-- lists are empty, it will use the corresponding default value
+zipWithDefault :: (a -> b -> c) -> a -> b -> [a] -> [b] -> [c]
+zipWithDefault f a _ [] ys         = map (f a) ys
+zipWithDefault f _ b xs []         = map (`f` b) xs
+zipWithDefault f a b (x:xs) (y:ys) = (f x y) : (zipWithDefault f a b xs ys)
