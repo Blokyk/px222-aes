78e0d7d31c15e4b269a931534e9c53eba25aedf7
blokyk <eliot.courvoisier@gmail.com>
2023-04-05 09:19:56 +0200

    feat(hs): start work on polynomials

--- lib-chloride/src/Algebra/Polynomial.hs
+++ lib-chloride/src/Algebra/Polynomial.hs
@@ -1,7 +1,49 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use camelCase" #-}
 
 module Algebra.Polynomial where
 
+import Algebra.Ring
 import Algebra.Field
 
-class Field coeff => Polynomial t coeff where
+import Utils (zipWithDefault)
+
+newtype Polynomial a = Polynomial [a]
+
+degree :: Polynomial a -> Int
+degree (Polynomial []) = undefined
+degree (Polynomial a)  = length a - 1
+
+convolve :: Field a => [a] -> [a] -> [a]
+convolve xs ys
+    = do
+        x <- xs
+        mult x <$> ys
+
+nullPolynomial :: Polynomial a
+nullPolynomial = Polynomial []
+
+divEuclide :: Ring a => Polynomial a -> Polynomial a -> (Polynomial a, Polynomial a)
+divEuclide divided@(Polynomial a) divider@(Polynomial b)
+    | degree divider < degree divided = (nullPolynomial, divided)
+    | otherwise
+        = undefined
+
+mod_polynomial :: Ring a => Polynomial a -> Polynomial a -> Polynomial a
+mod_polynomial a b = snd (divEuclide a b)
+
+reduce :: Ring a => Polynomial a -> Polynomial a
+reduce = mod_polynomial irreducible_m
+
+irreducible_m :: Ring a => Polynomial a
+irreducible_m = Polynomial [one, one, zero, one, one, zero, zero, zero, one]
+
+instance (Field a) => Ring (Polynomial a) where
+    -- takes each coeff and adds them together; in case there's more coeffs on one side,
+    -- it will just use `zero` to 'pad' the shorter one with zeroes
+    add (Polynomial a) (Polynomial b) = Polynomial (zipWithDefault add zero zero a b)
+    zero = Polynomial []
+    add_inverse (Polynomial a) = Polynomial (map add_inverse a)
+    mult (Polynomial a) (Polynomial b) = Polynomial (convolve a b) `mod_polynomial` irreducible_m
+    one = Polynomial [one]
