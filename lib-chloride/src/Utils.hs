c04c442ad9b90d004d7d808696c7143865442702
blokyk <eliot.courvoisier@gmail.com>
2023-04-22 21:59:40 +0200

    chore(hs): add Utils.withIndex

--- lib-chloride/src/Utils.hs
+++ lib-chloride/src/Utils.hs
@@ -1,5 +1,6 @@
 module Utils (
       zipWithDefault
+    , withIndex
     , padLeft
 ) where
 
@@ -9,6 +10,9 @@ zipWithDefault :: (a -> b -> c) -> a -> b -> [a] -> [b] -> [c]
 zipWithDefault f a _ [] ys         = map (f a) ys
 zipWithDefault f _ b xs []         = map (`f` b) xs
 zipWithDefault f a b (x:xs) (y:ys) = (f x y) : (zipWithDefault f a b xs ys)
+
+withIndex :: [a] -> [(a, Int)]
+withIndex l = zip l [0..]
 
 padLeft :: Int -> a -> [a] -> [a]
 padLeft n x xs = replicate (n - length xs) x ++ xs
